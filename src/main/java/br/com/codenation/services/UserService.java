50bbab81f99805b322dac551b07c96854afcd596
Adson Gregorio <adson.gregorio@gmail.com>
2020-07-12 20:17:53 -0300

    Removendo implementação de specifications

--- src/main/java/br/com/codenation/services/UserService.java
+++ src/main/java/br/com/codenation/services/UserService.java
@@ -1,16 +1,12 @@
 package br.com.codenation.services;
 
-import static br.com.codenation.specifications.GenericSpecificationBuilder.filterRecords;
-
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import br.com.codenation.entities.User;
 import br.com.codenation.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService extends BaseService<UserRepository, User, UUID> {
@@ -27,7 +23,4 @@ public class UserService extends BaseService<UserRepository, User, UUID> {
 		});
 	}
 
-	public List<User> findWithFilters(Map<Class<?>, Class<?>> params) {
-		return repository.findAll(filterRecords(params));
-	}
 }
